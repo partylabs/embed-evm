1fd66b020495be731b3947ac2b2a4f58d83ae2a4
nasadorian <narek.asadorian@sap.com>
2023-12-18 14:51:00 -0800

    Start bridging FFI

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,25 @@
 pub mod xen;
 
-use ethers::{abi::Address, prelude::EthEvent};
+use ethers::{abi::Address, prelude::{EthEvent, Transaction}, types::{Signature, U256}};
 use async_trait::async_trait;
 
+#[swift_bridge::bridge]
+pub mod ffi {
+
+  #[swift_bridge(swift_repr="struct")]
+  pub struct TransactionJson {
+    json: String,
+  }
+
+  #[swift_bridge(swift_repr="struct")]
+  pub struct SignedTransactionJson {
+    json: String,
+    signature: String,
+  }
+}
+
 #[async_trait]
-pub trait Stakeable {
+pub trait StakingTransaction {
   type Token;
   type TermUnits;
   type Err;
